ff4413c969539903e2f4be67b9c0f9a2f267dcf9
Nicolas Pastor Bueno <nicolas@MacBook-Pro-de-Nicolas.local>
2021-10-23 22:00:22 -0300

    chore: add new parameters to method to plot correctly

--- src/aves/visualization/treemap/treemap.py
+++ src/aves/visualization/treemap/treemap.py
@@ -2,11 +2,37 @@ import matplotlib.pyplot as plt
 import squarify
 import pandas as pd
 
-def treemap(df: pd.DataFrame, groupByColumn: str, aggregatedColumn: str, aggregatedFunction):
-    res = df.groupby(by=groupByColumn).agg(
-        d_max=pd.NamedAgg(column=aggregatedColumn, aggfunc=aggregatedFunction)
-    ).reset_index(drop=True)
+def drawTreeMapSumAggr(df: pd.DataFrame, group_by_column: str, aggregated_column: str, axis: bool, legend_class: str):
+    drawTreemap(df, group_by_column, aggregated_column, "sum", axis, legend_class)
 
-    squarify.plot(res['d_max'].tolist())
+def drawTreemap(
+    df: pd.DataFrame,
+    group_by_column: str,
+    aggregated_column: str,
+    aggregated_function: str,
+    axis: bool,
+    legend_class: str):
+    res = df.groupby(by=group_by_column).agg(
+        param=pd.NamedAgg(column=aggregated_column, aggfunc=aggregated_function)
+    ).reset_index()
+
+    plot_legend(res, group_by_column, legend_class)
+
+    squarify.plot(
+        sizes = res['param'],
+        label=  res['label'],
+        alpha = 0.6,
+        pad = True)
+
+    if not axis:
+        plt.axis('off')
+
     plt.show()
 
+def plot_legend(df: pd.DataFrame, group_by_column: str, legend_class: str) :
+    if legend_class == "legend_class_aggr":
+        df['label'] = df[group_by_column].astype(str) + "(" + df['param'].astype(str) + ")"
+    elif legend_class == "legend_class" :
+        df['label'] = df[group_by_column].astype(str)
+    else :
+        df['label'] = ""
